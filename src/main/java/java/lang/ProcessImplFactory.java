39f0ea133c580bf85e5c2ed89e4e900e81c137a7
Alexander Kurtakov <akurtako@redhat.com>
2019-10-30 17:15:24 +0200

    Remove unused imports.
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- src/main/java/java/lang/ProcessImplFactory.java
+++ src/main/java/java/lang/ProcessImplFactory.java
@@ -9,9 +9,7 @@
  **********************************************************************/
 package java.lang;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
